5b57f2af9db21a84a33d17888f0aee22e815495c
Lexicon <Lexicon@LAPTOP-SQ8C44R4>
2021-12-26 19:31:43 +0100

    Add project files.

---
+++ UnitTest.calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest.calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest.calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1b45068-8614-48d6-b9ff-cee3f6b3d4dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest.calculator/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest.calculator
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Fact]
+        public void MultiplicationMustReturnNotNullValue()
+        {
+            var calculator = new Calculator();
+
+            Xunit.Assert.Equal(8, calculator.Multiplication(2, 4));
+        }
+
+        [Fact]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Xunit.Assert.True(calculator.Subtraction(2, 4) == -2);
+        }
+
+        [Fact]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Xunit.Assert.True(calculator.Division(2, 4) == 0);
+        }
+
+        [Fact]
+        public void DivisionMustThrowException()
+        {
+            var calculator = new Calculator();
+
+            Xunit.Assert.Throws<DivideByZeroException>(() => calculator.Division(2, 0));
+        }
+
+        [Test]
+        public void AddAlwaysReturnsExpectedValue()
+        {
+            var calculatorTest = new Calculator();
+
+        }
+    }
+}
+    }
+}
